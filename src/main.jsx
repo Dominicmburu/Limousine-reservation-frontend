c855b9e25e542ed859b2004f40c087b120fee515
nvanderwalt <nvanderwalt12@yahoo.com>
2025-03-23 23:41:12 +0300

    limo frontend v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './Styles/styles.css';
-import './Styles/variables.css';
-import './Styles/responsive.css';
+import './styles/styles.css';
+import './styles/variables.css';
+import './styles/responsive.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
